c7223494b168e0550fe53a2d3e665700bdee189a
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-06-20 23:41:33 +0100

    Update order route

--- api/order/notification.py
+++ api/order/notification.py
@@ -5,10 +5,14 @@ from api.order import app
 def send_order_notification(order_id: UUID, user_id: UUID, status, datetime_sent):
     '''Function to send order notification to a user'''
     
-    app.socketio.emit('order_status_change', {
+    data =  {
         'order_id': order_id,
         'user_id': user_id,
         'status': status,
         'datetime_sent': datetime_sent,
-    })
+    }
+    app.socketio.emit('order_status_change', data)
+    
+    print('Order notification sent')
+    print(data)
     
